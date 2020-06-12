02973c7fe235ee1f4cf5c0aebf2c9e656b309322
brian <brianyauu@gmail.com>
2020-06-12 18:36:54 +0800

    tmp

--- config.h
+++ config.h
@@ -4,20 +4,21 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-  "Source Code Pro:pixelsize=16:antialias=true:autohint=true",
-  "NotoCJK:pixelsize=16:antialias=true:autohint=true",
-  "JoyPixels:pixelsize=16:antialias=true:autohint=true",
-  "Symbola:pixelsize=16:antialias=true:autohint=true"
+  "Source Code Pro:pixelsize=15:antialias=true:autohint=true",
+  "NotoCJK:pixelsize=15:antialias=true:autohint=true",
+  "JoyPixels:pixelsize=15:antialias=true:autohint=true",
+  "Symbola:pixelsize=15:antialias=true:autohint=true",
+  "Symbols Nerd Font:pixelsize=16:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  /*     fg         bg       */
+  [SchemeNorm] = { "#bbbbbb", "#222222" },
+  [SchemeSel]  = { "#eeeeee", "#005577" },
+  [SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
