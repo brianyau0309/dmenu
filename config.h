e7c86d379db540b4e21dac269726fed8a22b6753
Brian Yau <brianyauu@gmail.com>
2021-02-26 16:25:45 +0800

    backup

--- config.h
+++ config.h
@@ -5,11 +5,11 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-  "CascadiaCode:pixelsize=15:antialias=true:autohint=true",
-  "NotoSansCJKHK:pixelsize=15:antialias=true:autohint=true",
-  "JoyPixels:pixelsize=15:antialias=true:autohint=true",
-  "Symbola:pixelsize=15:antialias=true:autohint=true",
-  "Symbols Nerd Font:pixelsize=16:antialias=true:autohint=true"
+  "CascadiaCode:pixelsize=14:antialias=true:autohint=true",
+  "NotoSansCJKHK:pixelsize=14:antialias=true:autohint=true",
+  "JoyPixels:pixelsize=14:antialias=true:autohint=true",
+  "Symbola:pixelsize=14:antialias=true:autohint=true",
+  "Symbols Nerd Font:pixelsize=15:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
